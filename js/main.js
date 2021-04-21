e8be49b806193175d88fed5cf5f8c99cc3f2c074
bagues33 <bagustofan@yahoo.com>
2021-04-21 23:34:32 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+const responsive = {
+	0: {
+		items: 1
+	},
+
+	320: {
+		items:1
+	},
+	560: {
+		items: 2
+	},
+	960: {
+		items: 3
+	}
+
+}
+
 $(document).ready(function(){
 	
 	$nav = $('.nav');
@@ -6,7 +23,7 @@ $(document).ready(function(){
 	$toggleCollapse.click(function(){
 		$nav.toggleClass('collapse');
 	});
-});
+
 
 $(".owl-carousel").owlCarousel({
 	loop: true,
@@ -14,6 +31,18 @@ $(".owl-carousel").owlCarousel({
 	autoplayTimeout: 3000,
 	dots: false,
 	nav: true,
-	navText: [$(".owl-navigation .owl-nav-prev"),$(".owl-navigation .owl-nav-next")]
+	navText: [$(".owl-navigation .owl-nav-prev"),$(".owl-navigation .owl-nav-next")],
+	responsive: responsive,
+	});
+
+$('.move-up span').click(function(){
+	$('html,body').animate({
+		scrollTop: 0
+	}, 1000);
+
+	});
+
+AOS.init();
 
 });
+
